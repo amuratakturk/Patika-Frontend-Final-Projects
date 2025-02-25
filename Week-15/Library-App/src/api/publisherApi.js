0a3678b451f8aec9866e8a4551495a4753ee36b5
Abdurrahman Murat Aktürk <muratakturk97@hotmail.com>
2025-02-25 13:46:27 +0300

    last commit

--- Week-15/Library-App/src/api/publisherApi.js
+++ Week-15/Library-App/src/api/publisherApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_BASE_URL + "api/v1/publishers"; // Endpoint URL
+const BASE_URL = import.meta.env.VITE_BASE_URL + "/api/v1/publishers"; // Endpoint URL
 
 // Get Categories (GET)
 export const  getPublishers = async () => {
